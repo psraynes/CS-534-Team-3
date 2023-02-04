20719c8e7091d0058a49eb7df83442715dfba419
Paul <Paul@DESKTOP-6URSMA2>
2023-02-04 15:39:11 -0500

    Create file headers. added code from search.py

---
+++ Group Project 1/SimpleProblemSolvingAgent.py
@@ -0,0 +1,44 @@
+# A Simple Problem Solving Agent
+# Takes a graph and can fine a path between 2 points via
+# either Greedy Best-First Search or A*
+# Basic architecture taken from Artificial Intelligence: A Modern Approach reference code
+# Repo located at https://github.com/aimacode/aima-python
+
+from graph import Graph, UndirectedGraph
+
+class SimpleProblemSolvingAgentProgram:
+    """
+    [Figure 3.1]
+    Abstract framework for a problem-solving agent.
+    """
+
+    def __init__(self, initial_state=None):
+        """State is an abstract representation of the state
+        of the world, and seq is the list of actions required
+        to get to a particular state from the initial state(root)."""
+        self.state = initial_state
+        self.seq = []
+
+    def __call__(self, percept):
+        """[Figure 3.1] Formulate a goal and problem, then
+        search for a sequence of actions to solve it."""
+        self.state = self.update_state(self.state, percept)
+        if not self.seq:
+            goal = self.formulate_goal(self.state)
+            problem = self.formulate_problem(self.state, goal)
+            self.seq = self.search(problem)
+            if not self.seq:
+                return None
+        return self.seq.pop(0)
+
+    def update_state(self, state, percept):
+        raise NotImplementedError
+
+    def formulate_goal(self, state):
+        raise NotImplementedError
+
+    def formulate_problem(self, state, goal):
+        raise NotImplementedError
+
+    def search(self, problem):
+        raise NotImplementedError
