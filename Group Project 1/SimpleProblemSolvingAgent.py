3f958c12b46c63104e0c3607c6af3a1bb07801f3
Paul Raynes <psraynes@gmail.com>
2023-02-09 20:35:24 -0500

    clean up spsa search and clarify other functions

--- Group Project 1/SimpleProblemSolvingAgent.py
+++ Group Project 1/SimpleProblemSolvingAgent.py
@@ -4,29 +4,28 @@
 # Basic architecture taken from Artificial Intelligence: A Modern Approach reference code
 # Repo located at https://github.com/aimacode/aima-python
 
-from graph import Graph, UndirectedGraph
+from graph import Graph, UndirectedGraph, Node
 from search import best_first_graph_search, astar_search
 
 
 class SimpleProblemSolvingAgent:
     """
-    [Figure 3.1]
-    Abstract framework for a problem-solving agent.
+    Abstract framework for a problem-solving agent, modified to
+    be a graph searching agent.
     """
 
     def __init__(self, initial_state=None, search_type=best_first_graph_search):
-        """State is an abstract representation of the state
-        of the world, and seq is the list of actions required
-        to get to a particular state from the initial state(root).
-        Search_type is the type of search we want to perform. This
+        """State is a location in the graph, and seq is the list 
+        of locations required to get to a particular state from the initial
+        state(root). Search_type is the type of search we want to perform. This
         can be changed after initialization."""
         self.state = initial_state
         self.seq = []
         self.search_type = search_type
 
     def __call__(self, percept):
-        """[Figure 3.1] Formulate a goal and problem, then
-        search for a sequence of actions to solve it."""
+        """Formulate a goal and problem, then search for a sequence of actions
+        to solve it. Returns a list of locations representing the path"""
         self.state = self.update_state(self.state, percept)
         if not self.seq:
             goal = self.formulate_goal(self.state)
@@ -34,19 +33,30 @@ class SimpleProblemSolvingAgent:
             self.seq = self.search(problem)
             if not self.seq:
                 return None
-        return self.seq.pop(0)
+        return self.seq
 
     def update_state(self, state, percept):
+        # For our use case, this will be setting the goal of the search
         raise NotImplementedError
 
     def formulate_goal(self, state):
+        # For our use case, this will be reading the goal of the search
         raise NotImplementedError
 
     def formulate_problem(self, state, goal):
+        # Problem created here will be a GraphProblem
         raise NotImplementedError
 
     def search(self, problem):
-        return self.search_type(problem)
+        results = self.search_type(problem.h, problem)
+         # Result will be a Node representing the end of the path or None if no path
+        if results is None:
+            return None
+        else:
+            path = []
+            for result in results.path():
+                path.append(result)
+
 
     def set_search_type(self, search_type):
         if (search_type == best_first_graph_search or search_type == astar_search):
