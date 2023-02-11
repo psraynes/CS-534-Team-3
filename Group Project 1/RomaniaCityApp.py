2256094f6095e036c4dff1471682ed618e4b8ee8
Will Godsey <willgodsey@gmail.com>
2023-02-11 17:31:03 -0500

    start on app, read cities and create spsa

--- Group Project 1/RomaniaCityApp.py
+++ Group Project 1/RomaniaCityApp.py
@@ -1,9 +1,18 @@
 # Main Romania City App
 
-from SimpleProblemSolvingAgent import SimpleProblemSolvingAgentProgram
+from SimpleProblemSolvingAgent import SimpleProblemSolvingAgent
+from graph import romania_map
+
 
 def main():
-    pass
+    starting_city = input("Please enter the name of your starting city: ")
+    destination_city = input("Please enter the name of your desired destination city: ")
+
+    spsa = SimpleProblemSolvingAgent( romania_map, starting_city)
+
+    desired_path = spsa(destination_city)
+    print(desired_path)
+    return 0
 
 
 if __name__ == "__main__":
