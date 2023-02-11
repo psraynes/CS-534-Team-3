18001ad67fabd9ce8f642c6d2cd8058b34e62153
Will Godsey <willgodsey@gmail.com>
2023-02-11 17:40:42 -0500

    adapt app to new constructor. add astar search

--- Group Project 1/RomaniaCityApp.py
+++ Group Project 1/RomaniaCityApp.py
@@ -1,17 +1,26 @@
 # Main Romania City App
 
-from SimpleProblemSolvingAgent import SimpleProblemSolvingAgent
+from SimpleProblemSolvingAgent import SimpleProblemSolvingAgent, astar_search
 from graph import romania_map
 
 
 def main():
+    # Read the cities from the user
     starting_city = input("Please enter the name of your starting city: ")
     destination_city = input("Please enter the name of your desired destination city: ")
 
-    spsa = SimpleProblemSolvingAgent( romania_map, starting_city)
+    # Create the SPSA object from the map loaded earlier and the starting city
+    state_dict = {"graph": romania_map, "initial": starting_city}
+    spsa = SimpleProblemSolvingAgent(state_dict)
 
-    desired_path = spsa(destination_city)
-    print(desired_path)
+    # By default, the SPSA object uses best-first search. Call it by passing the desitination
+    best_first_path = spsa(destination_city)
+    print(best_first_path)
+
+    # Change to A* search and call it
+    spsa.set_search_type(astar_search)
+    astar_path = spsa(destination_city)
+    print(astar_path)
     return 0
 
 
