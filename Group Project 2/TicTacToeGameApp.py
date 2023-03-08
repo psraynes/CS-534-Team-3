a60943f4d8a3d1626727c3f930f5dde97692a248
Paul Raynes <psraynes@gmail.com>
2023-03-08 16:06:28 -0500

    Create game.py and tictactoe bases

---
+++ Group Project 2/TicTacToeGameApp.py
@@ -0,0 +1,11 @@
+# Main TicTacToe App
+
+from TicTacToeClass import TicTacToe
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+    
---
+++ Group Project 2/game.py
@@ -0,0 +1,229 @@
+# Game object, related classes, and game based searches
+# Majority of this code is pulled from the Artificial Intelligence: A Modern Approach reference code
+# Repo located at https://github.com/aimacode/aima-python
+
+import random
+import numpy as np
+from collections import namedtuple
+
+GameState = namedtuple('GameState', 'to_move, utility, board, moves')
+
+
+class Game:
+    """A game is similar to a problem, but it has a utility for each
+    state and a terminal test instead of a path cost and a goal
+    test. To create a game, subclass this class and implement actions,
+    result, utility, and terminal_test. You may override display and
+    successors or you can inherit their default methods. You will also
+    need to set the .initial attribute to the initial state; this can
+    be done in the constructor."""
+
+    def actions(self, state: GameState):
+        """Return a list of the allowable moves at this point."""
+        raise NotImplementedError
+
+    def result(self, state: GameState, move):
+        """Return the state that results from making a move from a state."""
+        raise NotImplementedError
+
+    def utility(self, state: GameState, player):
+        """Return the value of this final state to player."""
+        raise NotImplementedError
+
+    def terminal_test(self, state: GameState):
+        """Return True if this is a final state for the game."""
+        return not self.actions(state)
+
+    def to_move(self, state: GameState):
+        """Return the player whose move it is in this state."""
+        return state.to_move
+
+    def display(self, state: GameState):
+        """Print or otherwise display the state."""
+        print(state)
+
+    def __repr__(self):
+        return '<{}>'.format(self.__class__.__name__)
+
+    def play_game(self, *players):
+        """Play an n-person, move-alternating game."""
+        state = self.initial
+        while True:
+            for player in players:
+                move = player(self, state)
+                state = self.result(state, move)
+                if self.terminal_test(state):
+                    self.display(state)
+                    return self.utility(state, self.to_move(self.initial))
+
+
+class StochasticGame(Game):
+    """A stochastic game includes uncertain events which influence
+    the moves of players at each state. To create a stochastic game, subclass
+    this class and implement chances and outcome along with the other
+    unimplemented game class methods."""
+
+    def chances(self, state: GameState):
+        """Return a list of all possible uncertain events at a state."""
+        raise NotImplementedError
+
+    def outcome(self, state: GameState, chance):
+        """Return the state which is the outcome of a chance trial."""
+        raise NotImplementedError
+
+    def probability(self, chance):
+        """Return the probability of occurrence of a chance."""
+        raise NotImplementedError
+
+    def play_game(self, *players):
+        """Play an n-person, move-alternating stochastic game."""
+        state = self.initial
+        while True:
+            for player in players:
+                chance = random.choice(self.chances(state))
+                state = self.outcome(state, chance)
+                move = player(self, state)
+                state = self.result(state, move)
+                if self.terminal_test(state):
+                    self.display(state)
+                    return self.utility(state, self.to_move(self.initial))
+
+
+# Players
+def query_player(game: Game, state: GameState):
+    """Make a move by querying standard input."""
+    print("current state:")
+    game.display(state)
+    print("available moves: {}".format(game.actions(state)))
+    print("")
+    move = None
+    if game.actions(state):
+        move_string = input('Your move? ')
+        try:
+            move = eval(move_string)
+        except NameError:
+            move = move_string
+    else:
+        print('no legal moves: passing turn to next player')
+    return move
+
+
+def random_player(game: Game, state: GameState):
+    """A player that chooses a legal move at random."""
+    return random.choice(game.actions(state)) if game.actions(state) else None
+
+
+def alpha_beta_player(game: Game, state: GameState):
+    return alpha_beta_search(state, game)
+
+
+def expect_min_max_player(game: Game, state: GameState):
+    return expect_minmax(state, game)
+
+
+# Searches
+def minmax_decision(state: GameState, game: Game):
+    """Given a state in a game, calculate the best move by searching
+    forward all the way to the terminal states. [Figure 5.3]"""
+
+    player = game.to_move(state)
+
+    def max_value(state: GameState):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = -np.inf
+        for a in game.actions(state):
+            v = max(v, min_value(game.result(state, a)))
+        return v
+
+    def min_value(state: GameState):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = np.inf
+        for a in game.actions(state):
+            v = min(v, max_value(game.result(state, a)))
+        return v
+
+    # Body of minmax_decision:
+    return max(game.actions(state), key=lambda a: min_value(game.result(state, a)))
+
+
+def expect_minmax(state: GameState, game: Game):
+    """
+    [Figure 5.11]
+    Return the best move for a player after dice are thrown. The game tree
+	includes chance nodes along with min and max nodes.
+	"""
+    player = game.to_move(state)
+
+    def max_value(state: GameState):
+        v = -np.inf
+        for a in game.actions(state):
+            v = max(v, chance_node(state, a))
+        return v
+
+    def min_value(state: GameState):
+        v = np.inf
+        for a in game.actions(state):
+            v = min(v, chance_node(state, a))
+        return v
+
+    def chance_node(state: GameState, action):
+        res_state = game.result(state, action)
+        if game.terminal_test(res_state):
+            return game.utility(res_state, player)
+        sum_chances = 0
+        num_chances = len(game.chances(res_state))
+        for chance in game.chances(res_state):
+            res_state = game.outcome(res_state, chance)
+            util = 0
+            if res_state.to_move == player:
+                util = max_value(res_state)
+            else:
+                util = min_value(res_state)
+            sum_chances += util * game.probability(chance)
+        return sum_chances / num_chances
+
+    # Body of expect_min_max:
+    return max(game.actions(state), key=lambda a: chance_node(state, a), default=None)
+
+
+def alpha_beta_search(state: GameState, game: Game):
+    """Search game to determine best action; use alpha-beta pruning.
+    As in [Figure 5.7], this version searches all the way to the leaves."""
+
+    player = game.to_move(state)
+
+    # Functions used by alpha_beta
+    def max_value(state: GameState, alpha, beta):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = -np.inf
+        for a in game.actions(state):
+            v = max(v, min_value(game.result(state, a), alpha, beta))
+            if v >= beta:
+                return v
+            alpha = max(alpha, v)
+        return v
+
+    def min_value(state: GameState, alpha, beta):
+        if game.terminal_test(state):
+            return game.utility(state, player)
+        v = np.inf
+        for a in game.actions(state):
+            v = min(v, max_value(game.result(state, a), alpha, beta))
+            if v <= alpha:
+                return v
+            beta = min(beta, v)
+        return v
+
+    # Body of alpha_beta_search:
+    best_score = -np.inf
+    beta = np.inf
+    best_action = None
+    for a in game.actions(state):
+        v = min_value(game.result(state, a), best_score, beta)
+        if v > best_score:
+            best_score = v
+            best_action = a
+    return best_action
