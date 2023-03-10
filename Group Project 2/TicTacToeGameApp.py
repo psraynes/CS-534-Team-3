585b9dbfde7c42de716d211e47befe9cdc97d336
Paul Raynes <psraynes@gmail.com>
2023-03-09 20:09:34 -0500

    Flesh Out App

--- Group Project 2/TicTacToeGameApp.py
+++ Group Project 2/TicTacToeGameApp.py
@@ -5,8 +5,33 @@ from game import random_player, query_player
 
 
 def main():
-    ttt = TicTacToe()
-    ttt.play_game(random_player, query_player)
+    while True:
+        # Query the user for the size of the board
+        size = input("Please input a board size in the form HxV:")
+        size_list = size.split("x")
+        
+        # TODO: Verify that the input size is valid aka numbers and >3
+        
+        # Create the TicTacToe object
+        ttt = TicTacToe(int(size_list[0]), int(size_list[1]))
+        result = ttt.play_game(random_player, random_player)
+        
+        # Display Results
+        if result == 1:
+            print("X Wins!")
+        elif result == -1:
+            print("O Wins!")
+        else:
+            print("Draw!")
+        
+        # Query if the user wants to keep searching this map
+        want_to_go_again = input("Would you like to continue to play Tic-Tac-Toe? Y/N: ")
+        if want_to_go_again.casefold() == "y":
+            # If they do, we restart this loop
+            continue
+        else:
+            print("Thank You for Using Our App!")
+            break
     
 
 if __name__ == "__main__":
