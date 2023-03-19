0c6a2493e8827d18c725294d2b49499de25f9ac7
Lenore <lenoreogren@gmail.com>
2023-03-18 22:18:49 -0400

    Standardized variable labels in new functions. Removed Todos

--- Group Project 2/search.py
+++ Group Project 2/search.py
@@ -111,26 +111,24 @@ def astar_search(problem, h=None, display=False):
     return best_first_graph_search(problem, lambda n: n.path_cost + h(n), display)
 
 
-# Todo: Add Hill Climbing Search
 def hill_climbing(problem):
     """
     [Figure 4.2]
     From the initial node, keep choosing the neighbor with highest value,
     stopping when no neighbor is better.
     """
-    current = Node(problem.initial)
+    node = Node(problem.initial)
     while True:
-        neighbors = current.expand(problem)
+        neighbors = node.expand(problem)
         if not neighbors:
             break
         neighbor = argmax_random_tie(neighbors, key=lambda node: problem.value(node.state))
-        if problem.value(neighbor.state) <= problem.value(current.state):
+        if problem.value(neighbor.state) <= problem.value(node.state):
             break
-        current = neighbor
-    return current.state
+        node = neighbor
+    return node.state
 
 
-# Todo: Add Simulated Annealing Search and exp_schedule
 def exp_schedule(k=20, lam=0.005, limit=100):
     """One possible schedule function for simulated annealing"""
     return lambda t: (k * np.exp(-lam * t) if t < limit else 0)
@@ -139,15 +137,15 @@ def exp_schedule(k=20, lam=0.005, limit=100):
 def simulated_annealing(problem, schedule=exp_schedule()):
     """[Figure 4.5] CAUTION: This differs from the pseudocode as it
     returns a state instead of a Node."""
-    current = Node(problem.initial)
+    node = Node(problem.initial)
     for t in range(sys.maxsize):
         T = schedule(t)
         if T == 0:
-            return current.state
-        neighbors = current.expand(problem)
+            return node.state
+        neighbors = node.expand(problem)
         if not neighbors:
-            return current.state
+            return node.state
         next_choice = random.choice(neighbors)
-        delta_e = problem.value(next_choice.state) - problem.value(current.state)
+        delta_e = problem.value(next_choice.state) - problem.value(node.state)
         if delta_e > 0 or probability(np.exp(delta_e / T)):
-            current = next_choice
+            node = next_choice
