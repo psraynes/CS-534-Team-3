ad0b23bf423b04bc5a4910b8801d9d5f8124a140
LenOgren <lenoreogren@gmail.com>
2023-04-08 17:10:58 -0400

    Added data loading and sampling function.

--- Group Project 3/MachineFailurePredictorPIpeline.py
+++ Group Project 3/MachineFailurePredictorPIpeline.py
@@ -1,7 +1,13 @@
 from imblearn.under_sampling import RandomUnderSampler
+import pandas as pd
+
+from data_processing import *
+
 
 def main():
-    rus = RandomUnderSampler(replacement=True)
+    data = load_and_sample('ai4i2020.csv', 'Machine failure')
+
+    print(data.shape)
     
 
 if __name__ == "__main__":
