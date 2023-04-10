8861891a02dd6fa19b15c64d940d3bc504335e1b
Paul Raynes <psraynes@gmail.com>
2023-04-10 19:22:29 -0400

    print tables

--- Group Project 3/MachineFailurePredictorPIpeline.py
+++ Group Project 3/MachineFailurePredictorPIpeline.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
-from sklearn.ensemble import BaggingClassifier, AdaBoostClassifier,RandomForestClassifier
+from sklearn.ensemble import BaggingClassifier, AdaBoostClassifier,RandomForestClassifier\
 
 from data_processing import *
 
@@ -31,8 +31,10 @@ def main():
                 'activation': ['identity', 'logistic', 'tanh', 'relu']
                 }
     
-    mlp_search = GridSearchCV(mlp, mlp_grid, n_jobs=-1, cv=5)
+    mlp_search = GridSearchCV(mlp, mlp_grid, scoring='f1', n_jobs=-1, cv=5)
     mlp_search.fit(features_train, class_train)
+    mlp_params = mlp_search.best_params_
+    mlp_score = mlp_search.score(features_train, class_train)
 
     # Support Vector Machine. Tuning on the regularization parameter and kernel function:
     # C, kernel
@@ -41,8 +43,10 @@ def main():
                 'kernel': ['linear', 'poly', 'rbf', 'sigmoid']
                 }
 
-    svm_search = GridSearchCV(svm, svm_grid, n_jobs=-1, cv=5)
+    svm_search = GridSearchCV(svm, svm_grid, scoring='f1', n_jobs=-1, cv=5)
     svm_search.fit(features_train, class_train)
+    svm_params = svm_search.best_params_
+    svm_score = svm_search.score(features_train, class_train)
     
     # Bagging Classifier with Decision Tree Classifier used. Tune the tree count and observations:
     # n_estimators, max_samples
@@ -51,8 +55,10 @@ def main():
                 'max_samples': [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
                 }
 
-    bag_search = GridSearchCV(bag, bag_grid, n_jobs=-1, cv=5)
+    bag_search = GridSearchCV(bag, bag_grid, scoring='f1', n_jobs=-1, cv=5)
     bag_search.fit(features_train, class_train)
+    bag_params = bag_search.best_params_
+    bag_score = bag_search.score(features_train, class_train)
     
     # Ada Boost Classifier with Decision Trees. Tune tree count and learning rate:
     # n_estimators, learning_rate
@@ -61,8 +67,10 @@ def main():
                 'learning_rate': [0.1, 0.5, 1, 5, 10, 50]
                 }
 
-    ada_search = GridSearchCV(ada, ada_grid, n_jobs=-1, cv=5)
+    ada_search = GridSearchCV(ada, ada_grid, scoring='f1', n_jobs=-1, cv=5)
     ada_search.fit(features_train, class_train)
+    ada_params = ada_search.best_params_
+    ada_score = ada_search.score(features_train, class_train)
     
     # Random Forest Classifier with Decision Trees. Tune tree count, splitting criterion, features used, tree depth, and
     # observations used in each tree:
@@ -75,10 +83,29 @@ def main():
                 'max_samples': [n for n in np.linspace(0.1, 1, 10)]
                 }
 
-    rdf_search = RandomizedSearchCV(rdf, rdf_grid, n_iter=50, n_jobs=-1, cv=5, error_score='raise')
+    # We perform this one as a randomized search instead of exhaustive to maintain a reasonable run time.
+    rdf_search = RandomizedSearchCV(rdf, rdf_grid, n_iter=500, scoring='f1', n_jobs=-1, cv=5)
     rdf_search.fit(features_train, class_train)
+    rdf_params = rdf_search.best_params_
+    rdf_score = rdf_search.score(features_train, class_train)
     
-    print(data.shape)
+    print("{:<30} {:<64} {:<24}".format('ML Trained Model','Best Parameter Values','F1 Score on Training Data'))
+    print()
+    print("{:<30} {:<64} {:<24}".format('Artificial Neural Networks',str(mlp_params),mlp_score))
+    print("{:<30} {:<64} {:<24}".format('Support Vector Machine',str(svm_params),svm_score))
+    print("{:<30} {:<64} {:<24}".format('BaggingClassifier',str(bag_params),bag_score))
+    print("{:<30} {:<64} {:<24}".format('AdaBoost',str(ada_params),ada_score))
+    print("{:<30} {:<64} {:<24}".format('Random Forest ',str(rdf_params),rdf_score))
+    
+    print()
+    
+    print("{:<30} {:<64} {:<24}".format('ML Trained Model','Best Parameter Values','F1 Score on Test Data'))
+    print()
+    print("{:<30} {:<64} {:<24}".format('Artificial Neural Networks',str(mlp_params),mlp_search.score(features_test, class_test)))
+    print("{:<30} {:<64} {:<24}".format('Support Vector Machine',str(svm_params),svm_search.score(features_test, class_test)))
+    print("{:<30} {:<64} {:<24}".format('BaggingClassifier',str(bag_params),bag_search.score(features_test, class_test)))
+    print("{:<30} {:<64} {:<24}".format('AdaBoost',str(ada_params),ada_search.score(features_test, class_test)))
+    print("{:<30} {:<64} {:<24}".format('Random Forest ',str(rdf_params),rdf_search.score(features_test, class_test)))
     
 
 if __name__ == "__main__":
