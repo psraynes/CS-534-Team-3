4ed56187c2acf319ba542094c0ce922a6fe6d23b
Paul Raynes <psraynes@gmail.com>
2023-04-17 17:44:30 -0400

    clean up loading, put into function

--- Main Project/CorrositonDetection.py
+++ Main Project/CorrositonDetection.py
@@ -2,61 +2,49 @@ import cv2
 from skimage.feature import graycomatrix, graycoprops
 import numpy as np
 
-# todo: code for loading all images
+def loadRawImage(path):
+    picture = cv2.imread(path)
 
-path = "C:/Users/Paul/Documents/AI Corrosion Pics/Corrosion Condition State Classification/Corrosion Condition State Classification/512x512/Train/images_512/0.jpeg"
-picture = cv2.imread(path)
+    # convert to hsv and grayscale
+    hsv_img = cv2.cvtColor(picture, cv2.COLOR_BGR2HSV)
+    grayscale_img = cv2.cvtColor(picture, cv2.COLOR_BGR2GRAY)
 
+    # Break image into 16x16 patches -> 32 patches per side -> 1024 patches total
+    imgheight = grayscale_img.shape[0]
+    imgwidth = grayscale_img.shape[1]
 
-# convert to hsv
-hsv_img = cv2.cvtColor(picture, cv2.COLOR_BGR2HSV)
+    patch_per_side = 32
+    patch_height = imgheight//patch_per_side
+    patch_widtth = imgwidth//patch_per_side
 
-grayscale_img = cv2.cvtColor(picture, cv2.COLOR_BGR2GRAY)
+    tiles = np.empty(shape=(imgheight//patch_height, imgwidth//patch_widtth, patch_height, patch_widtth), dtype='int')
 
-# Break image into 16x16 patches -> 32 patches per side -> 1024 patches total
-imgheight = grayscale_img.shape[0]
-imgwidth = grayscale_img.shape[1]
+    for x in range(imgheight):
+        for y in range(imgwidth):
+            tiles[x//patch_height][y//patch_widtth][x % patch_height][y % patch_widtth] = grayscale_img[x][y]
+            
+    # Generate a GLCM for each patch
+    num_glcm_props = 2 # This represents the number of different glcm properties we read, used to make expanding that number easier
+    glcm_data = np.empty(shape=(patch_per_side, patch_per_side, num_glcm_props*4), dtype=float)
 
-patch_per_side = 32
+    for x in range(patch_per_side):
+        for y in range(patch_per_side):
+            # We create a glcm for a distance of 1 in 4 direction representing N, NE, E, SE
+            glcm = graycomatrix(tiles[x][y], [1], [0, np.pi/4, np.pi/2, 3*np.pi/4], levels=256)
+            
+            contrast = graycoprops(glcm, 'contrast')
+            correlation = graycoprops(glcm, 'correlation')
 
-M = imgheight//patch_per_side
-N = imgwidth//patch_per_side
+            for i in range(0,num_glcm_props*4,num_glcm_props):
+                glcm_data[x][y][i] = contrast[0][i//num_glcm_props]
+                glcm_data[x][y][i+1] = correlation[0][i//num_glcm_props]
+                
+    # Combine HSV and GLCM data into one object
+    data = np.empty(shape=(hsv_img.shape[0],hsv_img.shape[1],11))
 
-img = grayscale_img
-
-img_ht = imgheight
-img_wd = imgwidth
-tile_ht = M
-tile_wd = N
-
-tiles = np.empty(shape=(img_ht//tile_ht, img_wd//tile_wd, tile_ht, tile_wd), dtype='int')
-
-for x in range(img_ht):
-    for y in range(img_wd):
-        tiles[x//tile_ht][y//tile_wd][x % tile_ht][y % tile_wd] = img[x][y]
-
-print(tiles[0][0])
-print(grayscale_img)
-# cv2.imshow('img', grayscale_img)
-# cv2.waitKey(0)
-
-glcm_data = np.empty(shape=(patch_per_side, patch_per_side, 8), dtype=float)
-
-for x in range(patch_per_side):
-    for y in range(patch_per_side):
-        glcm = graycomatrix(tiles[x][y], [1], [0, np.pi/4, np.pi/2, 3*np.pi/4], levels=256)
+    for x in range(hsv_img.shape[0]):
+        for y in range(hsv_img.shape[1]):
+            data[x][y] = np.concatenate((hsv_img[x][y], glcm_data[x//patch_height][y//patch_widtth]))
+            
+    return data
         
-        contrast = graycoprops(glcm, 'contrast')
-        correlation = graycoprops(glcm, 'correlation')
-
-        for i in range(0,8,2):
-            glcm_data[x][y][i] = contrast[0][i//2]
-            glcm_data[x][y][i+1] = correlation[0][i//2]
-
-data = np.empty(shape=(hsv_img.shape[0],hsv_img.shape[1],11))
-
-for x in range(hsv_img.shape[0]):
-    for y in range(hsv_img.shape[1]):
-        data[x][y] = np.concatenate((hsv_img[x][y], glcm_data[x//M][y//N]))
-        
-print(data)
