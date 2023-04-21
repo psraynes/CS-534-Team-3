88aefa2a020002e20d0aa50ab8f4d3f6e11e78a0
Paul Raynes <psraynes@gmail.com>
2023-04-21 19:58:59 -0400

    load training and testing data as dataset

--- Group Project 4/AlexNetPredictorPipeline.py
+++ Group Project 4/AlexNetPredictorPipeline.py
@@ -1,5 +1,7 @@
 import os
-from sklearn.model_selection import train_test_split
+from torch.utils import data
+from torchvision import datasets
+import torchvision.transforms as transforms
 
 melanoma_folder = input("Please provide the directory for the Melanoma pictures: ")
 naevus_folder = input("Please provide the directory for the Naevus pictures: ")
@@ -8,8 +10,15 @@ if not melanoma_folder:
 if not naevus_folder:
     naevus_folder = "Group Project 4/complete_mednode_dataset/naevus/"
 
-melanoma_files = os.listdir(melanoma_folder)
-naevus_files = os.listdir(naevus_folder)
+melanoma_dataset = datasets.ImageFolder(melanoma_folder)
+naevus_dataset = datasets.ImageFolder(naevus_folder)
 
-melanoma_train, melanoma_test, naevus_train, naevus_test = train_test_split(melanoma_files, naevus_files, train_size=50)
+melanoma_trainset = data.Subset(melanoma_dataset, range(50))
+melanoma_testset = data.Subset(melanoma_dataset, range(50,70))
+
+naevus_trainset = data.Subset(naevus_dataset, range(50))
+naevus_testset = data.Subset(naevus_dataset, range(50,70))
+
+train_set = data.ConcatDataset([melanoma_trainset, naevus_trainset])
+test_set = data.ConcatDataset([melanoma_testset, naevus_testset])
 
